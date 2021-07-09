26f9170fea921aee9439dc8d8d4ab6fced620a23
dennis9352 <85466642+dennis9352@users.noreply.github.com>
2021-07-10 00:05:24 +0900

    conflict
    conflict

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
-import express from "express";
-import http from "http";
-import dotenv from "dotenv";
-import router from "./api/index.js";
-import io from "./chat";
+import express from 'express'
+import http from 'http'
+import dotenv from 'dotenv'
+import router from './api/index.js'
 
-dotenv.config();
-const app = express();
-const server = http.createServer(app);
+dotenv.config()
+const app = express()
+const server = http.createServer(app)
 
-app.use(router);
+app.use(router)
 server.listen(3000, () => {
-  console.log("서버가 요청을 받을 준비가 됐어요");
-});
+    console.log('서버가 요청을 받을 준비가 됐어요');
+  });
+
