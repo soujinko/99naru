d99043abb26a969278181123ae62882568e5f57e
SeungBinKim <seungbin0508@gmail.com>
2021-07-09 20:52:47 +0900

    Add router in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import http from 'http'
 import dotenv from 'dotenv'
+import router from './api/index.js'
 
 dotenv.config()
 const app = express()
 const server = http.createServer(app)
 
+app.use(router)
 server.listen(3000, () => {
     console.log('서버가 요청을 받을 준비가 됐어요');
   });
