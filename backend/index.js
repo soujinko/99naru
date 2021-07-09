23592d0b85e40409b2639950b3c69283df88696d
SeungBinKim <seungbin0508@gmail.com>
2021-07-10 00:40:29 +0900

    Hotfix 2

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,17 @@ import express from 'express'
 import http from 'http'
 import dotenv from 'dotenv'
 import router from './api/index.js'
+import cors from 'cors'
 
 dotenv.config()
 const app = express()
 const server = http.createServer(app)
-
+app.use(
+	cors({
+		method: ['GET', 'POST'],
+		credentials: true
+	})
+)
 app.use(router)
 server.listen(3000, () => {
 	console.log('서버가 요청을 받을 준비가 됐어요')
