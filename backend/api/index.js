5dd463d68070d16282edce5de65e179e335b7341
SeungBinKim <seungbin0508@gmail.com>
2021-07-09 14:51:40 +0900

    Set routers in api/index.js
    Routers for posts and comments

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,10 @@
 import express from 'express'
+import postsRouter from './posts.js'
+import commentsRouter from './comments.js'
 
 const router = express.Router()
+
+router.use('/posts', postsRouter)
+router.use('/comments', commentsRouter)
 
 export default router
