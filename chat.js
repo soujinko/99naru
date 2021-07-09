c07fde3fed7c84b3055ef2560396d97bf412fe1b
dennis9352 <85466642+dennis9352@users.noreply.github.com>
2021-07-09 16:45:41 +0900

    Update chat.js
    현재시간을 위한 moment, moment-timezone 모듈 설치

--- chat.js
+++ chat.js
@@ -1,1 +1,48 @@
 const socketIo = require("socket.io");
+const socketIo = require("socket.io");
+const io = socketIo(http);
+const moment = require("moment");
+require("moment-timezone");
+moment.tz.setDefault("Asia/Seoul");
+
+io.on("connection", (socket) => {
+  const currentDate = moment().format("YYYY-MM-DD HH:mm:ss"); //현재 시간
+  const joinedId = [];
+
+  socket.on("join", ({ userInfo }) => {
+    const nickname = userInfo.nickname;
+    const userSocketId = socket.id;
+
+    if (ids.indexOf(userSocketId) == -1) {
+      ids.push(userSocketId);
+      socket.emit("enterUser", `${nickname}님이 입장하셨습니다.`);
+    }
+  });
+
+  socket.on("sendMsg", (data) => {
+    const message = {
+      sendMsg: data.message,
+      nickname: data.user.nickname,
+      date: currentDate,
+    };
+    io.emit("receiveMsg", message);
+  });
+
+  const currentOn = [];
+  currentOn.push(nickname);
+
+  io.emit("currentOn", currentOn); // 현재 접속자 리스트
+
+  // 게시물 포스팅 알람
+  socket.on("posting", (data) => {
+    const post = {
+      nickname: data.nickname,
+      date: currentDate,
+    };
+    io.emit("notificationAlarm", post);
+  });
+
+  socket.on("disconnect", () => {
+    ids.splice(ids.indexOf(userSocketId), 1);
+  });
+});
