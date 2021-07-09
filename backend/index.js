8c82914950088f99f853658e436add9052d54d85
dennis9352 <85466642+dennis9352@users.noreply.github.com>
2021-07-09 23:42:32 +0900

    update chat
    server setting

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,8 @@
 import express from "express";
 import http from "http";
 import dotenv from "dotenv";
+import io from "./chat";
 
 dotenv.config();
 const app = express();
 const server = http.createServer(app);
-const socketio = require("./chat");
-const io = socketio(server);
