8f83e73a8d9156e0a057d16defaff165c57626ae
SeungBinKim <seungbin0508@gmail.com>
2021-07-09 15:27:28 +0900

    Initialize /models/index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,1 +1,23 @@
 import mongoose from 'mongoose'
+
+const connect = () => {
+	if (process.env.NODE_ENV !== 'production') {
+		mongoose.set('debug', true)
+	}
+}
+
+mongoose.connect('mongodb://localhost:27017/admin', {
+	dbName: 'board2',
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+}, err => {
+	if (err) console.error('몽고디비 연결 오류', err)
+	else console.log('몽고디비 연결 성공')
+})
+
+mongoose.connection.on('error', err => {
+	console.error('몽고디비 연결 에러', err)
+	connect()
+})
+
+connect()
