ef50637e98538c923935b1a97c4a8f0c9252406a
SeungBinKim <seungbin0508@gmail.com>
2021-07-09 15:20:12 +0900

    Initialize /api/comment.js

--- backend/api/comments.js
+++ backend/api/comments.js
@@ -2,4 +2,17 @@ import express from 'express'
 
 const router = express.Router()
 
+router.put('/:commentId', (req, res, next) => {
+
+})
+router.delete('/:commentId', (req, res, next) => {
+	// dont' actually delete the comment from db but set isDelete true
+	// By deleting the comment, it may cause descendant comments to be orphans
+})
+router.get('/', (req, res, next) => {
+
+})
+router.post('/', (req, res, next) => {
+
+})
 export default router
