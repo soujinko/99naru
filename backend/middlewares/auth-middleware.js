9f23346d61be4e9337c5d0b85ca43b86aa5aced9
soujinko <soujinko@naver.com>
2021-07-10 14:45:07 +0900

    auth middleware

--- backend/middlewares/auth-middleware.js
+++ backend/middlewares/auth-middleware.js
@@ -1,26 +1,26 @@
-// const jwt = require("jsonwebtoken");
-// const { User } = require("../models");
+import jwt from "jsonwebtoken";
+import User from "../models/user";
 
-// module.exports = (req, res, next) => {
-//   const { authorization } = req.headers;
-//   const [tokenType, tokenValue] = authorization.split(" ");
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  const [tokenType, tokenValue] = authorization.split(" ");
 
-//   if (tokenType !== "Bearer") {
-//     res.status(401).send({
-//       errorMessage: "로그인 후 사용하세요",
-//     });
-//     return;
-//   }
-//   try {
-//     const { userId } = jwt.verify(tokenValue, "sujin-secret-key");
-//     User.findByPk(userId).then((user) => {
-//       res.locals.user = user;
-//       next();
-//     });
-//   } catch (error) {
-//     res.status(401).send({
-//       errorMessage: "로그인 후 사용하세요",
-//     });
-//     return;
-//   }
-// };
+  if (tokenType !== "Bearer") {
+    res.status(401).send({
+      errorMessage: "로그인 후 사용하세요",
+    });
+    return;
+  }
+  try {
+    const { userId } = jwt.verify(tokenValue, process.env.SECRET_KEY);
+    User.findById(userId).then((user) => {
+      res.locals.user = user;
+      next();
+    });
+  } catch (error) {
+    res.status(401).send({
+      errorMessage: "로그인 후 사용하세요",
+    });
+    return;
+  }
+};
