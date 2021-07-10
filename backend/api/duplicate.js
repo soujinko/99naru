604ae5c3c2f62607068ecb725a226c49a5ca3282
SeungBinKim <seungbin0508@gmail.com>
2021-07-10 10:38:52 +0900

    Delete redundant use of middleware multer() in /api/duplicate.js

--- backend/api/duplicate.js
+++ backend/api/duplicate.js
@@ -4,7 +4,7 @@ import User from '../models/user.js'
 const router = express.Router();
 
 //   loginId, nickname 회원가입 중복체크
-router.post("/", multer.none(), async (req, res) => {
+router.post("/", async (req, res) => {
   const { loginId, nickname } = req.body;
   const existUser = await User.find({
     $and: [{ $or: [{ nickname }, { loginId }] }],
