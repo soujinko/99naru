751394f9abb8b75ff338e33d857634824363c9e3
soujinko <soujinko@naver.com>
2021-07-10 11:11:10 +0900

    no message

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,16 +1,16 @@
-import express from 'express'
-import postsRouter from './posts'
-import commentsRouter from './comments'
-import signupRouter from './signup'
-import signinRouter from './signin'
-import duplicateRouter from './duplicate'
+import express from "express";
+import postsRouter from "./posts";
+import commentsRouter from "./comments";
+import signupRouter from "./signup";
+import signinRouter from "./signin";
+import duplicateRouter from "./duplicate";
 
-const router = express.Router()
+const router = express.Router();
 
-router.use('/posts', postsRouter)
-router.use('/comments', commentsRouter)
-router.use('/signin', signinRouter)
-router.use('/signup', signupRouter)
-router.use('/duplicate', duplicateRouter)
+router.use("/posts", postsRouter);
+router.use("/comments", commentsRouter);
+router.use("/signin", signinRouter);
+router.use("/signup", signupRouter);
+router.use("/duplicate", duplicateRouter);
 
-export default router
+export default router;
--- backend/middlewares/auth-middleware.js
+++ backend/middlewares/auth-middleware.js
@@ -1,26 +1,26 @@
-const jwt = require("jsonwebtoken");
-const { User } = require("../models");
+// const jwt = require("jsonwebtoken");
+// const { User } = require("../models");
 
-module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  const [tokenType, tokenValue] = authorization.split(" ");
+// module.exports = (req, res, next) => {
+//   const { authorization } = req.headers;
+//   const [tokenType, tokenValue] = authorization.split(" ");
 
-  if (tokenType !== "Bearer") {
-    res.status(401).send({
-      errorMessage: "로그인 후 사용하세요",
-    });
-    return;
-  }
-  try {
-    const { userId } = jwt.verify(tokenValue, "sujin-secret-key");
-    User.findByPk(userId).then((user) => {
-      res.locals.user = user;
-      next();
-    });
-  } catch (error) {
-    res.status(401).send({
-      errorMessage: "로그인 후 사용하세요",
-    });
-    return;
-  }
-};
+//   if (tokenType !== "Bearer") {
+//     res.status(401).send({
+//       errorMessage: "로그인 후 사용하세요",
+//     });
+//     return;
+//   }
+//   try {
+//     const { userId } = jwt.verify(tokenValue, "sujin-secret-key");
+//     User.findByPk(userId).then((user) => {
+//       res.locals.user = user;
+//       next();
+//     });
+//   } catch (error) {
+//     res.status(401).send({
+//       errorMessage: "로그인 후 사용하세요",
+//     });
+//     return;
+//   }
+// };
