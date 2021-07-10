855e6860a719ad6343434bb95191ac3eec2f0318
soujinko <soujinko@naver.com>
2021-07-10 12:03:01 +0900

    병합

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,18 +1,16 @@
-<<<<<<< HEAD
+// import express from "express";
+// import postsRouter from "./posts";
+// import commentsRouter from "./comments";
+// import signupRouter from "./signup";
+// import signinRouter from "./signin";
+// import duplicateRouter from "./duplicate";
+
 import express from "express";
-import postsRouter from "./posts";
-import commentsRouter from "./comments";
-import signupRouter from "./signup";
-import signinRouter from "./signin";
-import duplicateRouter from "./duplicate";
-=======
-import express from 'express'
-import postsRouter from './posts.js'
-import commentsRouter from './comments.js'
-import signupRouter from './signup.js'
-import signinRouter from './signin.js'
-import duplicateRouter from './duplicate.js'
->>>>>>> f0e16e3fe3cbbc8350305496005a385dea8971d3
+import postsRouter from "./posts.js";
+import commentsRouter from "./comments.js";
+import signupRouter from "./signup.js";
+import signinRouter from "./signin.js";
+import duplicateRouter from "./duplicate.js";
 
 const router = express.Router();
 
--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,22 @@
-import express from 'express'
-import http from 'http'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import router from './api/index.js'
+import express from "express";
+import http from "http";
+import dotenv from "dotenv";
+import cors from "cors";
+import router from "./api/index.js";
 
-import multer from 'multer'
-import './models/index.js'
+import multer from "multer";
+import "./models/index.js";
 
-dotenv.config()
-const app = express()
-const server = http.createServer(app)
+dotenv.config();
+const app = express();
+const server = http.createServer(app);
 
-app.use(cors())
-app.use(multer().none())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(cors());
+app.use(multer().none());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api', router)
+app.use("/api", router);
 server.listen(3000, () => {
-	console.log('서버가 요청을 받을 준비가 됐어요')
-})
-
+  console.log("서버가 요청을 받을 준비가 됐어요");
+});
