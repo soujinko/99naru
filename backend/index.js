a1733b89127a3688d2f8bc486156e29b1c24ad3b
soujinko <soujinko@naver.com>
2021-07-10 12:05:44 +0900

    no message

--- backend/index.js
+++ backend/index.js
@@ -18,10 +18,5 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api", router);
 server.listen(3000, () => {
-<<<<<<< HEAD
   console.log("서버가 요청을 받을 준비가 됐어요");
 });
-=======
-	console.log('서버가 요청을 받을 준비가 됐어요')
-})
->>>>>>> 2c9bbeb69e0d56f84a854768a088c7b3940bfdc8
