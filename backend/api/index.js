47ca84fc8f32591a15d00c8c439d9e5dde0cde37
SeungBinKim <seungbin0508@gmail.com>
2021-07-10 09:05:35 +0900

    Delete irrelevant router in /api/index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import multer from 'multer'
 import postsRouter from './posts'
 import commentsRouter from './comments'
 import signupRouter from './signup'
@@ -8,14 +7,10 @@ import duplicateRouter from './duplicate'
 
 const router = express.Router()
 
-router.use(multer().none())
 router.use('/posts', postsRouter)
 router.use('/comments', commentsRouter)
 router.use('/signin', signinRouter)
 router.use('/signup', signupRouter)
 router.use('/duplicate', duplicateRouter)
-
-router.use('/api/posts', postsRouter)
-router.use('/api/comments', commentsRouter)
 
 export default router
