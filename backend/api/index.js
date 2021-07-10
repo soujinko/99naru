85220d4534cfc5abc40bf37acf4c030859b8ec29
SeungBinKim <seungbin0508@gmail.com>
2021-07-10 10:35:21 +0900

    Fix missing extension in from keywords

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,9 +1,9 @@
 import express from 'express'
-import postsRouter from './posts'
-import commentsRouter from './comments'
-import signupRouter from './signup'
-import signinRouter from './signin'
-import duplicateRouter from './duplicate'
+import postsRouter from './posts.js'
+import commentsRouter from './comments.js'
+import signupRouter from './signup.js'
+import signinRouter from './signin.js'
+import duplicateRouter from './duplicate.js'
 
 const router = express.Router()
 
--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const server = http.createServer(app)
 app.use(multer().none())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
+app.get('/', path.join(path.resolve()))
 app.use(router)
 server.listen(3000, () => {
 	console.log('서버가 요청을 받을 준비가 됐어요')
