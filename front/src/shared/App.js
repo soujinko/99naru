e08715673704892b47f622fa229179b1d895e1bb
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2021-07-13 10:28:13 +0900

    Update App.js
    대규모 업데이트

--- front/src/shared/App.js
+++ front/src/shared/App.js
@@ -1,29 +1,49 @@
-import "./App.css";
 import React from "react";
 import styled from "styled-components";
 
-import { ConnectedRouter } from "connected-react-router";
-import { history } from "../redux/configureStore";
+// import { Text, Grid, Input, Image } from "./elements";
+import SideBar from "../components/SideBar";
+import ChattingBar from "../components/ChattingBar";
+import UserList from "../components/UserList";
 
-import { Login, Signup, Profile, Main } from "../pages";
+import PostHome from "../pages/PostHome";
+import ProfileHome from "../pages/ProfileHome";
+import SignInSide from "../pages/LoginHome";
 
 import { Route } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { actionCreators as postActions } from "../redux/modules/post";
 
-function App() {
+import { ConnectedRouter } from "connected-react-router";
+import { history } from "../redux/configureStore";
 
-
+const App = (props) => {
   return (
     <React.Fragment>
-        <ConnectedRouter history={history}>
-          <Route path="/" exact component={Main} />
-          <Route path="/login" exact component={Login} />
-          <Route path="/signup" exact component={Signup} />
-          <Route path="/user/:id" exact component={Profile} />
-        </ConnectedRouter>
+      <ConnectedRouter history={history}>
+        <Route path="/" exact component={SignInSide} />
+        <Container>
+          <SideBar />
+          <Route path="/main" exact component={PostHome} />
+          <Route path="/user" exact component={ProfileHome} />
+          <ChattingBar />
+          <UserList />
+        </Container>
+      </ConnectedRouter>
     </React.Fragment>
   );
-}
+};
+
+const Container = styled.div`
+  /* border: 1px solid #f7f9f9; */
+  background-color: #f7f9f9;
+  display: flex;
+  height: 100vh;
+  flex-direction: row;
+  justify-content: center;
+  /* align-items: stretch; */
+  box-sizing: border-box;
+  overflow: hidden;
+`;
 
 export default App;
