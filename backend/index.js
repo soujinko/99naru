d4b940a3219f30ff1010bd90f4d535f744f42e03
soujinko <soujinko@naver.com>
2021-07-13 19:26:32 +0900

    nothing changed

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,24 @@
-import express from 'express'
-import http from 'http'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import router from './api/index.js'
-import multer from 'multer'
-import { Server } from 'socket.io'
-import './models/index.js'
+import express from "express";
+import http from "http";
+import dotenv from "dotenv";
+import cors from "cors";
+import router from "./api/index.js";
+import multer from "multer";
+import { Server } from "socket.io";
+import "./models/index.js";
 
-dotenv.config()
-const app = express()
-const server = http.createServer(app)
-export const io = new Server(server)
+dotenv.config();
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
 
-app.use(cors())
-app.use(multer().none())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(cors());
+app.use(multer().none());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api', router)
+app.use("/api", router);
 
 server.listen(process.env.PORT || 3000, () => {
-	console.log('서버 연결')
-})
+  console.log("서버 연결");
+});
