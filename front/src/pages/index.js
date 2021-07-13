982138e5f1dab41181ab16b7f06b9bce388d9287
SeungBinKim <seungbin0508@gmail.com>
2021-07-14 04:50:47 +0900

    Delete Main.js

--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -1,6 +1,5 @@
 import Login from "./Login";
 import Signup from "./Signup";
-import Main from "./Main";
 import Profile from "./Profile";
 
-export { Login, Signup, Main, Profile };
+export { Login, Signup, Profile };
