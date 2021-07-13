cb861f16358b785b141a1ac1de71ec6e49363e81
soujinko <soujinko@naver.com>
2021-07-13 19:23:43 +0900

    social login api added

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,18 +1,20 @@
-import express from 'express'
-import auth from '../middlewares/auth-middleware.js'
-import postsRouter from './posts.js'
-import commentsRouter from './comments.js'
-import signupRouter from './signup.js'
-import signinRouter from './signin.js'
-import duplicateRouter from './duplicate.js'
+import express from "express";
+import auth from "../middlewares/auth-middleware.js";
+import postsRouter from "./posts.js";
+import commentsRouter from "./comments.js";
+import signupRouter from "./signup.js";
+import signinRouter from "./signin.js";
+import duplicateRouter from "./duplicate.js";
+import socialRouter from "./social_signup.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.use('/signin', signinRouter)
-router.use('/signup', signupRouter)
-router.use(auth)
-router.use('/posts', postsRouter)
-router.use('/comments', commentsRouter)
-router.use('/duplicate', duplicateRouter)
+router.use("/signin", signinRouter);
+router.use("/signup", signupRouter);
+router.use(auth);
+router.use("/posts", postsRouter);
+router.use("/comments", commentsRouter);
+router.use("/duplicate", duplicateRouter);
+router.use("/social", socialRouter);
 
-export default router
+export default router;
