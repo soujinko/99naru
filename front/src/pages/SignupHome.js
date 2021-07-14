7f87bda79c60d3ac95c7f9cf25b6262ac9f487e7
sungsu <chltjdtn9703@gmail.com>
2021-07-14 23:55:17 +0900

    modify signup page

--- front/src/pages/SignupHome.js
+++ front/src/pages/SignupHome.js
@@ -1,21 +1,16 @@
 import React, { useState, useRef } from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 import { useDispatch } from "react-redux";
 import { actionCreators as userActions } from "../redux/modules/user";
-
+import {IdCheck, NickCheck, pwdCheck} from "../shared/common";
+import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -158,10 +153,10 @@ export default function SignupSide() {
               fullWidth
               name="password"
               label="Password"
-            //   type="password"
+              type="password"
               id="password"
               autoComplete="current-password"
-              inputProps = {{ pattern: "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]"}}
+              // inputProps = {{ pattern: "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]"}}
               //todo 여기랑 함수 패턴이 중복돼요
             />
             <TextField
@@ -172,7 +167,7 @@ export default function SignupSide() {
               fullWidth
               name="password"
               label="Password"
-            //   type="password"
+              type="password"
               id="password"
               autoComplete="current-password"
             />
