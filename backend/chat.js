fec86f032bed5df467c9bb008c844f20b1809cbf
dennis9352 <85466642+dennis9352@users.noreply.github.com>
2021-07-15 00:25:33 +0900

    remove unused functions
    remove unused functions socket server side

--- backend/chat.js
+++ backend/chat.js
@@ -1,8 +1,5 @@
 import { Server } from 'socket.io'
-import jwt from 'jsonwebtoken'
-import User from './models/user.js'
 import Chat from './models/chat.js'
-import dotenv from "dotenv"
 import server from './index.js'
 
 const io = new Server(server, {
@@ -35,11 +32,11 @@ const showChatLog = async () => {
 
 const currentOn = [];
 const currentOnUserInfo=[];
-dotenv.config()
+
 io.on("connection", (socket) => {
   deleteMaxChat()
   showChatLog()
-  const currentDate = JSON.stringify(new Date());
+
   io.emit('currentOn', currentOn); // (현재 접속자 리스트) 게시물 업데이트때문에 refresh 할일이 많아서 처음에 넣어줌.
 
   socket.on("join", ({ nickname }) => {
@@ -64,12 +61,6 @@ io.on("connection", (socket) => {
 
   //메세지 주고받기(프론트: 보내는 유저info, 보내는 메세지)
   socket.on("sendMsg", async ({message, nickname, date}) => {
-
-    const chatlog = {
-      messages : message,
-      nickname : nickname,
-      date : date,
-    }
     try{
       const maxOrder = await Chat.findOne({}).sort('-order').exec();
       let order = 1;
@@ -79,77 +70,12 @@ io.on("connection", (socket) => {
       }
       await Chat.create({ order, message, nickname, date })
       io.emit("receiveMsg", {nickname, message, date});
-    // io.emit('receiveMsg', chatlog);
+
     }catch (err) {
       console.error(err);
     };
   });
 
-  //귓속말(프론트: 보내는 유저info, 보내는 메세지, 받는 유저info)
-  socket.on("writingComment", (send, receive) => {
-    const sendUser = {
-        nickname : send.nickname,
-        sendMessage: send.message,
-        date: currentDate
-    };
-    for (let i in currentOnUserInfo){
-      if (currentOnUserInfo[i].nickname === receive.nickname){
-        const socketId = currentOnUserInfo[i].socketId;
-        socket.broadcast.to(socketId).emit("whisper", sendUser) //특정 socketid에게만 전송
-      }
-    }
-  });
-
-  // 게시물 포스팅 알람(프론트: 게시물 작성한 유저info)
-  socket.on("posting", (giveNickname) => {
-    const post = {
-      nickname: giveNickname,
-      date: currentDate,
-    };
-    io.emit("postNotification", post);
-  });
-
-  //댓글 작성 알람(프론트: 댓글 단 유저info, 댓글내용, 해당 게시물 유저info)
-  socket.on("writingComment", (post, comment) => {
-      const postUser = {
-          nickname : post.nickname
-      };
-      const commentUser = {
-        comment : comment.comment,
-        nickname : comment.nickname,
-        date: currentDate
-      };
-      for (let i in currentOnUserInfo){
-        if (currentOnUserInfo[i].nickname === receive.nickname){
-          const socketId = currentOnUserInfo[i].socketId;
-          socket.broadcast.to(socketId).emit("commentNotification", postUser, commentUser) //특정 socketid에게만 전송
-      }
-    }
-  });
-
-  //로그아웃 했을경우
-  socket.on('signOut', (token) => {
-    if (token === null) {
-      return;
-    }
-    const { userId } = jwt.verify(token, process.env.SECRET_KEY);
-
-    User.findById(userId)
-    .then((user) => {
-      const userInfo = user;
-
-      // 현재 접속중 배열에서 제거
-      currentOn.splice(currentOn.indexOf(userInfo.nickname), 1);
-
-      // 현재 socket.id랑 연결되어있는 닉네임이 있는 배열에서 제거
-      for (let i in currentOnUserInfo){
-        if (currentOnUserInfo[i].nickname === userInfo.nickname){
-          currentOnUserInfo.splice(i,1)
-        }
-      }
-      io.emit('exitUser', userInfo.nickname);
-      });
-  });
 
   socket.on("disconnect", () => {
     // 현재 socket.id랑 연결되어있는 닉네임이 있는 배열에서 제거
