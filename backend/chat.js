d2e865abef4ee8af7a32686202358288add369a2
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2021-07-14 22:44:15 +0900

    Add UserList
    기능만 구현

--- backend/chat.js
+++ backend/chat.js
@@ -42,35 +42,23 @@ io.on("connection", (socket) => {
   const currentDate = JSON.stringify(new Date());
   io.emit('currentOn', currentOn); // (현재 접속자 리스트) 게시물 업데이트때문에 refresh 할일이 많아서 처음에 넣어줌.
 
-  socket.on("join", ({ token }) => {
-    if (token === null) {
-      return;
-    }
-    const { userId } = jwt.verify(token, process.env.SECRET_KEY);
-
-    User.findById(userId)
-    .then((user) => {
-      const userInfo = user
+  socket.on("join", ({ nickname }) => {
+      const userNickname = nickname
       const userSocketId = {      // 특정 닉네임에게만 보내는 이벤트를 위한 socket.id저장
-        nickname : userInfo.nickname,
+        nickname : userNickname,
         socketId : socket.id
       }
-
-      if (currentOn.indexOf(userInfo.nickname) === -1) {  //현재 접속자에 유저아이디가 없으면 추가
-        currentOn.push(userInfo.nickname);
-        currentOnUserInfo.push(userSocketId);
-        io.emit('enterUser', userInfo.nickname);
+      if (currentOn.indexOf(userNickname) === -1) {  //현재 접속자에 유저아이디가 없으면 추가
+        currentOn.push(userNickname);
         io.emit('currentOn', currentOn); // 현재 접속자 리스트 업데이트
-
       }else{                      // refresh 할때마다 socket.id가 바뀌므로 같이 업데이트 해주는작업
         for (let i in currentOnUserInfo){
-          if (currentOnUserInfo[i].nickname === userInfo.nickname){
+          if (currentOnUserInfo[i].nickname === userNickname){
             currentOnUserInfo[i].socketId = userSocketId.socketId
           }
         }
       }
     });
-  });
 
 
   //메세지 주고받기(프론트: 보내는 유저info, 보내는 메세지)
@@ -163,6 +151,13 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnect", () => {
+    // 현재 socket.id랑 연결되어있는 닉네임이 있는 배열에서 제거
+    for (let i in currentOnUserInfo){
+      if (currentOnUserInfo[i].socketId === socket.id){
+        currentOn.splice(currentOn.indexOf(currentOnUserInfo[i].nickname), 1);
+        currentOnUserInfo.splice(currentOnUserInfo[i],1)
+      }
+    }
     console.log('나감');    // todo 브라우저를 끄거나 탭을 닫으면 disconnect 작동하는지 검사
   });
 });
