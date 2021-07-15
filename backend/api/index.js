09ea01037f406a5b7a9ddb7e7ad976a41a0336c2
soujinko <soujinko@naver.com>
2021-07-15 19:59:58 +0900

    no message

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,22 +1,21 @@
 import express from "express";
-import auth from "../middlewares/auth-middleware.js";
 import postsRouter from "./posts.js";
 import commentsRouter from "./comments.js";
 import signupRouter from "./signup.js";
 import signinRouter from "./signin.js";
 import duplicateRouter from "./duplicate.js";
 import socialRouter from "./social_signup.js";
-
+import auth from "../middlewares/auth-middleware.js";
 const router = express.Router();
 
+router.use("/google", socialRouter);
+router.use("/github", socialRouter);
+router.use("/kakao", socialRouter);
 router.use("/signin", signinRouter);
 router.use("/signup", signupRouter);
 router.use("/duplicate", duplicateRouter);
 router.use(auth);
 router.use("/posts", postsRouter);
 router.use("/comments", commentsRouter);
-router.use("/google", socialRouter);
-router.use("/github", socialRouter);
-router.use("/kakao", socialRouter);
 
 export default router;
